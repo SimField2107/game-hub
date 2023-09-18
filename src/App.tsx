08e750ef71db2a2eb5021a5434c27dab77859876
Seline Cornfield <Seline2107@googlemail.com>
2023-09-18 15:03:58 +0100

    Implement dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -19,11 +19,11 @@ function App() {
       <GridItem area="nav">
         <NavBar />
       </GridItem>
-      <Show above="lg">
-        <GridItem area="aside" bg="gold">
-          Aside
-        </GridItem>
-      </Show>
+      {/* <Show above="lg"> */}
+      <GridItem area="aside" bg="gold">
+        Aside
+      </GridItem>
+      {/* </Show> */}
       <GridItem area="main" bg="dodgerblue">
         Main
       </GridItem>
