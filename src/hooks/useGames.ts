6f9e29383220978f0ce80aee42206bce3c647838
Seline Cornfield <Seline2107@googlemail.com>
2023-09-20 14:58:17 +0100

    Displaying Platform Icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react";
 import APIClient from "../services/API-Client";
 
+
+ export interface Platform { 
+    id: number; 
+    name: string; 
+    slug: string; 
+
+}
 export interface Game {
     id: number;
     name: string;
     background_image: string; 
+    parent_platforms: { platform: Platform } []
   }
   
   interface FetchGamesResponse {
@@ -23,7 +31,7 @@ const useGames = () => {
         APIClient.get<FetchGamesResponse>("/games", {signal: controller.signal})
           .then((res) => setGames(res.data.results))
           .catch((err) => { 
-            if (err instanceof CanceledError) return; 
+            // if (err instanceof CanceledError) return; 
             setError(err.message)
           });
 
