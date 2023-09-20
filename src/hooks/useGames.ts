552f8e71498b789e2cea21fd7f64cc3d45fbc033
Seline Cornfield <Seline2107@googlemail.com>
2023-09-20 17:24:41 +0100

    Create a generice data fetching hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,5 @@
-import { useEffect, useState } from "react";
-import APIClient from "../services/API-Client";
-import {CanceledError} from "axios"; 
+
+import useData from "./useData";
 
 
 
@@ -18,39 +17,8 @@ export interface Game {
     metacritic: number; 
   }
   
-  interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-  }
 
-const useGames = () => { 
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState("");
-   const [isLoading, setLoading] = useState(false)
-    
-      useEffect(() => {
-
-        const controller = new AbortController(); 
-
-
-setLoading(true); 
-
-        APIClient.get<FetchGamesResponse>("/games", {signal: controller.signal})
-          .then((res) => {
-            setGames(res.data.results); 
-            setLoading(false); 
-          })
-          .catch((err) => { 
-            if (err instanceof CanceledError) return; 
-            setError(err.message)
-            setLoading(false); 
-          });
-
-          return () => controller.abort(); 
-      }, []);
-
-      return {games, error, isLoading}; 
-}
+const useGames = () => useData<Game>('/games'); 
 
 
 
