47a64d6474c2beb4a8e5127c5575936ff62f1aff
Seline Cornfield <Seline2107@googlemail.com>
2023-10-26 17:46:25 +0100

    Build Game Attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,4 +1,6 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publishers";
 
 
 
@@ -9,6 +11,8 @@ export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres: Genre[]; 
+  publishers: Publisher[]; 
   description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform; }[];
